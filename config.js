6ee42b5faa52795078c75fb5282be728c5dacc87
sg-webapps <smgeppert@gmx.net>
2026-09-11 23:26:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.HEIZOEL_HIRMER_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbziVUuLFjHZ_fV1ERLMKX9iHhkX_5nF_-FCaXHffsxq9Y3iL6IzpFGCxuyohmYs6gCwwQ/exec",
-  VERSION: "98"
+  VERSION: "99"
 };
